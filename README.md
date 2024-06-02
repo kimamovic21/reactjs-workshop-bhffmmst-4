abcbfde18cbbf6f279a176fafd1445fa8e9e036c
Dzmitry <dimaau1@gmail.com>
2024-06-02 13:30:15 +0300

    to-do list app

---
+++ README.md
@@ -0,0 +1,66 @@
+# To Do List
+
+## Deploying Vite App to GitHub Pages
+
+1.  **Create a GitHub Account and Install Git:**
+
+    - If you don't have a GitHub account, sign up and install Git on your computer.
+
+2.  **Create a New Repository:**
+
+    - Log in to your GitHub account and click on the "New" button to create a new repository.
+    - Set the repository name and click on the "Create repository" button.
+
+3.  **Set Up the Repository:**
+
+    Follow the command line instructions provided by GitHub to initialize the repository on your local machine.
+
+    ```bash
+    echo "# to-do-list-copy" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/DzmitryUr/to-do-list-copy.git
+    git push -u origin main
+    ```
+
+4.  **Configure the vite.config.js File**
+    Configure the vite.config.js file to set the correct base path.
+
+    ```javascript
+    export default defineConfig({
+      plugins: [react()],
+      base: /<REPOSITORY_NAME>/, // Replace <REPOSITORY_NAME> with your GitHub repository name
+    });
+    ```
+
+5.  **Install gh-pages**
+    Install gh-pages package as a dev dependency
+
+    ```bash
+    npm install gh-pages --save-dev
+    ```
+
+6.  **Update package.json**
+
+    - Set homepage in package.json by using URL on GihHub
+
+    ```json
+    "homepage": "https://{username}.github.io/{repo-name}/"
+    ```
+
+    - Add predeploy and deploy scripts to package.json
+
+    ```json
+    "scripts": {
+      //other scripts
+      "predeploy" : "npm run build",
+      "deploy" : "gh-pages -d dist",
+    }
+    ```
+
+7.  **Run Deploy**
+    ```bash
+    npm run deploy
+    ```
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import "./ToDoList.css";
+
+function ToDoList() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTask = () => {
+    if (!inputValue) {
+      alert("Please enter a valid task!");
+    }
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      task: inputValue,
+      completed: false,
+    };
+
+    console.log(newTodo);
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleDeleteTodo = (deleteId) => {
+    const newTodos = todos.filter((todo) => todo.id !== deleteId);
+    setTodos(newTodos);
+  };
+
+  const toggleTodoCompletion = (todoId) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container">
+      <h2>To Do List</h2>
+      <div className="row-add-task">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a new task"
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <div className="task-list">
+        {todos.map((todo) => {
+          return (
+            <div
+              className={`task-row ${todo.completed ? "completed" : ""}`}
+              key={todo.id}
+            >
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleTodoCompletion(todo.id)}
+              />
+              <div
+                className="task-text"
+                onClick={() => toggleTodoCompletion(todo.id)}
+              >
+                {todo.task}
+              </div>
+              <button
+                className="delete-btn"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ToDoList;
